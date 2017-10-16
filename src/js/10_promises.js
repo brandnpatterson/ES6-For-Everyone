3a1eec4a93d7dfa16b13e6c6be4f9c9c8f525f28
Brandon Patterson <brandon.patterson@sabre.com>
2017-10-16 11:11:29 -0500

    going through airbnb eslint

--- src/js/10_promises.js
+++ src/js/10_promises.js
@@ -1,3 +1,6 @@
+/* globals twttr ga */
+/* eslint-disable no-extend-disable */
+
 // const postsPromise = fetch('http://wesbos.com/wp-json/wp/v2/posts')
 //   .then(data => data.json())
 //   .then(data => console.log(data))
@@ -18,16 +21,47 @@
 //   })
 //
 
+// eslint disabled line per line
+const disableEslintOneRule = {
+  /* eslint-disable no-extend-native */
+  name: 'Brandon',
+  age: 29
+  /* eslint-enable no-extend-native */
+}
+
+const disableEslintAllRules = {
+  /* eslint-disable */
+  name: 'Brandon',
+  age: 29
+  /* eslint-enable */
+}
+
+
+const disableEslintThenEnable = {
+  /* eslint-disable */
+  name: 'Brandon',
+  age: 29,
+  /* eslint-enable */
+  sex: 'male',
+  town: 'Roanoke'
+}
+
 const posts = [
   { title: 'I love JavaScript', author_id: 34, id: 1 },
   { title: 'CSS!', author_id: 42, id: 2 },
-  { title: 'Dev tools tricks', author_id: 26, id: 3 }
+  { title: 'Dev tools tricks', author_id: 26, id: 3 },
 ];
 
 const authors = [
-  { name: 'Wes Bos', twitter: '@wesbos', bio: 'Canadian Developer', id: 34 },
-  { name: 'Chris Coyier', twitter: '@chriscoyier', bio: 'CSS Tricks and CodePen', id: 42 },
-  { name: 'Addy Osmani', twitter: '@addyosmani', bio: 'Googler', id: 26 }
+  {
+    name: 'Wes Bos', twitter: '@wesbos', bio: 'Canadian Developer', id: 34,
+  },
+  {
+    name: 'Chris Coyier', twitter: '@chriscoyier', bio: 'CSS Tricks and CodePen', id: 42,
+  },
+  {
+    name: 'Addy Osmani', twitter: '@addyosmani', bio: 'Googler', id: 26,
+  },
 ];
 
 function getPostById(id) {
@@ -35,7 +69,7 @@ function getPostById(id) {
   return new Promise((resolve, reject) => {
     // use a setTimeout to mimick a database
     setTimeout(() => {
-      const post = posts.find(post => post.id === id)
+      const post = posts.find(post => post.id === id);
       if (post) {
         resolve(post);
       } else {
@@ -50,15 +84,15 @@ function hydrateAuthor(post) {
   return new Promise((resolve, reject) => {
     // find the author
     const authorDetails = authors.find(person => person.id === post.author_id);
-    if(authorDetails) {
+    if (authorDetails) {
       // hydrate the post obj with the author object
       post.author = authorDetails;
-      delete post.author_id
+      delete post.author_id;
       resolve(post);
     } else {
       reject(Error('Cannot find the author'));
     }
-  })
+  });
 }
 
 // getPostById(1)
@@ -97,11 +131,13 @@ const streetCarsPromise = fetch('http://data.ratp.fr/api/datasets/1.0/search/?q=
 
 Promise
   .all([postsPromise, streetCarsPromise])
-  .then(responses => {
-    // there are many different types of data that can come back
-    // it isn't always JSON
-    return Promise.all(responses.map(res => res.json()))
-  })
-  .then(responses => {
-    console.log(responses);
-  })
+  .then(responses => Promise.all(responses.map(res => res.json())))
+// there are many different types of data that can come back
+// it isn't always JSON
+  .then((responses) => {
+    console.log(responses); 1;
+  });
+
+// globals set at the top of file
+ga.track();
+twttr.trackConversation();
--- src/js/1_new-variables.js
+++ src/js/1_new-variables.js
@@ -1,52 +1,52 @@
 // New Variables
 
 // var - can be redefined and redeclared - function scope
-var height = 100
-console.log(height)
+var height = 100;
+console.log(height);
 
 // redeclared, as well as redefined
-var height = 200
-console.log(height)
+var height = 200;
+console.log(height);
 
-var setWidth = () => {
-  var innerWidth = 300
-  console.log(innerWidth)
-}
-setWidth()
+const setWidth = () => {
+  const innerWidth = 300;
+  console.log(innerWidth);
+};
+setWidth();
 // console.log(innerWidth) // err
 
-var age = 100
+const age = 100;
 
 // let - cannot be redeclared, but are made to be redefined - block scope
 if (age > 12) {
-  let dogYears = age * 7
-  console.log(`You are ${dogYears} dog years old`)
+  const dogYears = age * 7;
+  console.log(`You are ${dogYears} dog years old`);
 }
 
 // block scoped
 for (let i = 0; i < 10; i++) {
   setTimeout(() => {
-    console.log(`The number is ${i}`)
-    console.log('Hello World')
-  }, 1000)
+    console.log(`The number is ${i}`);
+    console.log('Hello World');
+  }, 1000);
 }
 
 // const - cannot be redeclared or redefined
 const person = {
   name: 'Brandon',
-  age: 29
-}
+  age: 29,
+};
 
 // replaces iife because it is block scoped
 {
   const computer = {
     model: 'Mac',
-    year: 2017
-  }
-  console.log(computer)
+    year: 2017,
+  };
+  console.log(computer);
 }
 // console.log(computer) // err
 
 // temporal dead zone === You may not access a variable before it is defined
-console.log(pizza)
-var pizza = 'Deep Dish 🍕🍕🍕'
+console.log(pizza);
+var pizza = 'Deep Dish 🍕🍕🍕';
