5eeba1f5e9e981741bd09f3dff23323bb61195be
Brandon Patterson <brandnpatterson@gmail.com>
2017-10-16 15:30:11 -0500

    implemented modules with babel/webpack

---
+++ src/js/app.js
@@ -0,0 +1,14 @@
+// import './1_new-variables';
+// import './2_arrow';
+// import './2-arrow-ex';
+// import './3_template-string';
+// import './3-template-string-ex';
+// import './4_strings';
+// import './5_destructuring';
+// import './6_for-of';
+// import './7_array-find';
+// import './8_rest-operator';
+// import './9_object-literals';
+// import './10_promises';
+// import './11_symbols';
+import './12_modules';
